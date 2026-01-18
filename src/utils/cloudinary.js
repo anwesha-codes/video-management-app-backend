a47a63678aaf370cc3b580959d974fe6d668fcd5
anwesha02 <anweshaaditya2002@gmail.com>
2026-01-18 08:09:25 +0530

    made the cloudinary and multer settings for file upload

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { response } from "express"
+import { v2 as cloudinary } from cloudinary
+import fs from "fs"
+
+cloudinary.v2.config({
+    //putting random values for learning but since all these are private confidential details so we will move it to .env file
+    // cloud_name : "annyclouds",
+    // api_key: "234823748979",
+    // api_secret:'2323232323232323232323'
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //else upload file on cloudinary
+        await cloudinary.uploader.upload(localFilePath, {
+            resource_type: auto,
+        })
+        //file uploaded successfully
+        console.log("File is uploaded on cloudinary", response.url)
+        return response
+    }
+    catch (error) {
+        fs.unlinkSync(localFilePath) //remove the locally saved temporary file as the upload operationgot failed
+        return null
+    }
+}
