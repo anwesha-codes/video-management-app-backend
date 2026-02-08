24f1e3de4458dc0c9dbc7f8e6e2d69afae512455
anwesha02 <anweshaaditya2002@gmail.com>
2026-02-08 18:27:46 +0530

    add user account management routes: change password, get current user, and update account details

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,5 +22,7 @@ router.route("/login").post(loginUser)
 //secure routes - logout
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-access-token").post(refreshAccessToken)
-
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 export default router 
